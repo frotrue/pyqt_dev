d33c8b3d9da58f48b1e0eb3f6a68a079ceca9141
parkmujin <moojin0830@gmail.com>
2025-10-20 13:49:43 +0900

    .

--- gpt_ans.py
+++ gpt_ans.py
@@ -12,8 +12,8 @@ def get_gpt_response(prompt):
             {"role": "system", "content": """
 당신은 건축물 관리사입니다. YOLO의 추론결과를 보고 건축물의 위험도 및 추후 조치를 분석해주세요. 입력 형식은 다음과 같습니다.
 {
-spalling: 0.51
-ExposedRebars: 0.35
+classname: n
+classname: n
 }
 총 4개의 클래스가 있으며 다음과 같습니다. crack, corrosion, ExposedRebars, spalling.
 가중치를 부여할때 ExposedRebars > spalling > crack > corrosion 순으로 위험도가 높다고 판단하고 가중치를 부여합니다.
@@ -21,7 +21,7 @@ ExposedRebars: 0.35
 0.8 이상부터 심각한 결함으로 봅니다.
 위의 결과를 총합하여 다음과 같은 형식의 답변을 제공합니다.
 {
-위험도 : n%
+위험도 : n% 
 조치사항 : 
 }
 예시 답변은 다음과 같습니다.
